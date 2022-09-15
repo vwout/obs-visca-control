e342ee30dc9c2db3b0af3057ba71c02efaac70ab
vwout <github15@info.vwout.nl>
2022-09-15 19:43:25 +0200

    Determine timeouts according Visca spec - do not count timeouts for completion replies when ack is received

--- libvisca.lua
+++ libvisca.lua
@@ -710,14 +710,16 @@ function Visca.Transmission:add_reply(reply)
 end
 
 function Visca.Transmission:timed_out()
-    -- Only reply cycle should take maximum 4V. 1V is 42msec (worst case) for Sony.
+    -- One reply cycle should take maximum 4V. 1V is 42msec (worst case) for Sony.
     -- Including one 1V for sending, the timeout needed should only be 210000000ns.
-    local visca_timeout = 1500000000
+    -- This typically not met by Visca over IP, so an increased timeout is used.
+    local visca_timeout = 1000000000
     if not self.send_timestamp then
         return false
+    elseif self.send_timestamp and (self.ack_timestamp or self.error_timestamp or self.completion_timestamp) then
+        return false
     else
-        return (obs.os_gettime_ns() - math.max(self.send_timestamp, self.ack_timestamp or 0)) >
-            visca_timeout
+        return (obs.os_gettime_ns() - (self.send_timestamp or 0)) > visca_timeout
     end
 end
 
