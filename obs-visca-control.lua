1ece40b852670697b488addda6d7e77e959c81d8
vwout <github15@info.vwout.nl>
2022-09-15 19:49:51 +0200

    Release v2.0

--- obs-visca-control.lua
+++ obs-visca-control.lua
@@ -3,7 +3,7 @@ local Visca = require("libvisca")
 
 local plugin_info = {
     name = "Visca Camera Control",
-    version = "1.6",
+    version = "2.0",
     url = "https://github.com/vwout/obs-visca-control",
     description = "Camera control via Visca over IP",
     author = "vwout"
